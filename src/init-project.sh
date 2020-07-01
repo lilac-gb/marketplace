32cdeb95b4be89f57ec72f033bb6f23b1db5cd12
rogozhuk <resmedia@ya.ru>
2020-07-01 15:16:48 +0300

    #1 - fix SH init

--- src/init-project.sh
+++ src/init-project.sh
@@ -25,7 +25,7 @@ case "$ENV" in
         ;;
         "docker")
         wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet  > /dev/null
-        ./composer.phar install --no-interaction --prefer-dist --quiet && awk '{ sub("\r$", ""); print }' init > init2 && mv init2 init &&  ./init --env=Docker --overwrite=y && awk '{ sub("\r$", ""); print }' yii > yii2 && mv yii2 yii && ./yii migrate <<< "yes"
+        ./composer.phar install --no-interaction --prefer-dist --quiet &&  ./init --env=Docker --overwrite=y && ./yii migrate <<< "yes"
     ;;
 esac
 
