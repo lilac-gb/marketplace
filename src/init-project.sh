3bd6237d1d605dd95ca029eb8856edbdd754bc8f
rogozhuk <resmedia@ya.ru>
2020-07-01 15:06:47 +0300

    #1 - fix SH

--- src/init-project.sh
+++ src/init-project.sh
@@ -20,38 +20,12 @@ if ! [[ "$ENV" =~ ^(local|docker)$ ]]; then
 fi
 
 #switch next operations
-OS="`uname`"
-
-case $OS in 'Linux')
-    case "$ENV" in
-         "local")
-         ;;
-         "docker")
-         wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet  > /dev/null
-             ./composer.phar install --no-interaction --prefer-dist --quiet && ./init --env=Docker --overwrite=y && ./yii migrate <<< "yes"
-         ;;
-     esac
-    ;;
-    'WindowsNT')
-    case "$ENV" in
-           "local")
-            ;;
-            "docker")
-            wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet  > /dev/null
-            ./composer.phar install --no-interaction --prefer-dist --quiet && ./init --env=Docker --overwrite=y && awk '{ sub("\r$", ""); print }'     yii > yii2 && mv yii2 yii && ./yii migrate <<< "yes"
-        ;;
-    esac
-    ;;
-    'Darwin')
-    case "$ENV" in
-        "local")
+case "$ENV" in
+       "local")
         ;;
         "docker")
         wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet  > /dev/null
-            ./composer.phar install --no-interaction --prefer-dist --quiet && ./init --env=Docker --overwrite=y && ./yii migrate <<< "yes"
-        ;;
-    esac
-   ;;
+        ./composer.phar install --no-interaction --prefer-dist --quiet && awk '{ sub("\r$", ""); print }' init > init2 && mv init2 init &&  ./init --env=Docker --overwrite=y && awk '{ sub("\r$", ""); print }' yii > yii2 && mv yii2 yii && ./yii migrate <<< "yes"
+    ;;
 esac
-
 
