734e5f2b87ecced30c8ef289f950440f2f0c8509
rogozhuk <resmedia@ya.ru>
2020-07-01 13:08:24 +0300

    #1 - Fix init for php

--- src/init-project.sh
+++ src/init-project.sh
@@ -27,7 +27,8 @@ case "$ENV" in
         echo "$HOST_IP marketplace.docker" >> /etc/hosts
         echo "$HOST_IP api.marketplace.docker" >> /etc/hosts
         echo "$HOST_IP office.marketplace.docker" >> /etc/hosts
+        echo "$HOST_IP pma.marketplace.docker" >> /etc/hosts
         wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet  > /dev/null
-        ./composer.phar install --no-interaction --prefer-dist --quiet && ./init --env=Docker --overwrite=y && ./yii migrate <<< "yes"
+        ./composer.phar install --no-interaction --prefer-dist --quiet && ./yii migrate <<< "yes" # ./init --env=Docker --overwrite=y &&
     ;;
 esac
