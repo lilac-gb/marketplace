27b3904ed19d353fd640578422db5143b8a5ef08
rogozhuk <resmedia@ya.ru>
2020-07-05 20:39:27 +0300

    #16 - code fixes

--- src/frontend/pages/rules.vue
+++ src/frontend/pages/rules.vue
@@ -1,38 +1,44 @@
 <template>
-  <section>
-    <b-container>
-      <b-row style="min-height:48rem;" class="mt-5">
-        <b-col>
-          <h2>{{meta.data.success}}</h2>
+    <section>
+        <b-container>
+            <b-row style="min-height:48rem;" class="mt-5">
+                <b-col>
+                    <h2>{{content.name}}</h2>
 
-          <p>{{meta.data.description}}</p>
-
-          {{meta}}
-        </b-col>
-      </b-row>
-    </b-container>
-  </section>
+                    <div v-html="content.description"/>
+                </b-col>
+            </b-row>
+        </b-container>
+    </section>
 </template>
 
 <script>
-  export default {
-    async asyncData ({$axios}) {
-      let meta = await $axios.$get(`https://api.marketplace.docker/page/rules?expand=_metaTags&_format=json`)
-      return {meta}
-    },
-    data: () =>({
-      meta: []
-    }),
-    head() {
-      return {
-        title: this.meta.data.name,
-        meta: [
-          { redicolossimo: 'bla-bla-bla #2' } //локальные динамические метатеги
-        ],
-      };
-    },
+    import config from "../config";
 
-  }
+    export default {
+        async asyncData({$axios}) {
+            let items = await $axios.$get(`${config.api_url}/page${location.pathname}?expand=_metaTags&_format=json`);
+            const meta = items.data._metaTags;
+            const content = items.data;
+            return {meta, content}
+        },
+        data: () => ({
+            meta: [],
+            content: [],
+        }),
+        head() {
+            const {title, description, keywords} = this.meta;
+            return {
+                title: title,
+                meta: [
+                    {
+                        description: description,
+                        keywords: keywords
+                    } //локальные динамические метатеги
+                ],
+            };
+        },
+    }
 </script>
 
 <style scoped>
