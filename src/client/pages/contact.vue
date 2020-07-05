d4a2322a14311c116ed9108c480902143beaaa38
rogozhuk <resmedia@ya.ru>
2020-07-05 16:59:16 +0300

    fixes

---
+++ src/client/pages/contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-container>
+      <b-row style="min-height:48rem;" class="mt-5">
+        <b-col>
+          <h2>contact</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur.
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
