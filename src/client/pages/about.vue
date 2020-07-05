91a1d911b9fdeabfab4bf277551a8d5242d5c1f7
rogozhuk <resmedia@ya.ru>
2020-07-05 16:59:16 +0300

    fixes

---
+++ src/client/pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-container>
+      <b-row style="min-height:48rem;" class="mt-5">
+        <b-col>
+          <h2>Heading</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur.
+          </p>
+
+          <p>
+            <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation »</a>
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "About"
+    };
+  },
+};
+</script>
---
+++ src/client/pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-container>
+    <b-row style="min-height:34rem;" class="mt-5">
+      <b-col>
+        <h2>login</h2>
+        <p>
+          
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
