e6decb73036dc49702972546e1174606f77d8526
dannilsan <dannilsan@gmail.com>
2020-07-07 00:47:09 +0700

    #16 - update directory structure for dynamic routing

--- src/frontend/pages/_url.vue
+++ src/frontend/pages/_url.vue
@@ -4,7 +4,7 @@
       <b-row style="min-height:48rem;" class="mt-5">
         <b-col>
           <h1>Link {{$route.params.url}}</h1>
-          <h1>Link {{ link.id }}</h1>
+          <h1>Link {{ link }}</h1>
         </b-col>
       </b-row>
     </b-container>
@@ -15,12 +15,12 @@
   export default {
     title: "${$route.params.url}",
     data: () => ({
-      link: []
+      link: 'rules'
     }),
     async mounted() {
-      //this.link = await this.$axios.$get(`https://api.marketplace.docker/page/${this.$route.params.url}?expand=_metaTags&_format=json`)
-      let response = await this.$axios.$get(`https://jsonplaceholder.typicode.com/posts?_limit=1`)
-      this.link = response[0]
+      //this.link = await this.$axios.$get(`https://api.marketplace.docker/rules?expand=_metaTags`)
+      //let response = await this.$axios.$get(`https://jsonplaceholder.typicode.com/posts?_limit=1`)
+      //this.link = response[0]
       console.log(this.link)
     },
     // validate({params}) {
