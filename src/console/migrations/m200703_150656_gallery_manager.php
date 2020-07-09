cf3f720a233b25bceb855351a832c827a7b843a9
rogozhuk <resmedia@ya.ru>
2020-07-09 18:00:25 +0300

    #hotfix gallery migration

--- src/console/migrations/m200703_150656_gallery_manager.php
+++ src/console/migrations/m200703_150656_gallery_manager.php
@@ -1,5 +1,28 @@
 <?php
 
-class m200703_150656_gallery_manager extends zxbodya\yii2\galleryManager\migrations\m140930_003227_gallery_manager
+use yii\db\Migration;
+use yii\db\Schema;
+
+class m200703_150656_gallery_manager extends Migration
 {
+    public $tableName = '{{%gallery_image}}';
+
+    public function up()
+    {
+
+        $this->createTable($this->tableName, [
+                'id' => Schema::TYPE_PK,
+                'type' => Schema::TYPE_STRING,
+                'ownerId' => Schema::TYPE_STRING . ' NOT NULL',
+                'rank' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+                'name' => Schema::TYPE_STRING,
+                'description' => Schema::TYPE_TEXT,
+            ]
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable($this->tableName);
+    }
 }
