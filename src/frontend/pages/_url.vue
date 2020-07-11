3cfb57450d2930d94dd0a5e2b5c56fc1a831ba04
dannilsan <dannilsan@gmail.com>
2020-07-12 02:12:09 +0700

    #16 - complete all fixes

--- src/frontend/pages/_url.vue
+++ src/frontend/pages/_url.vue
@@ -3,10 +3,11 @@
     <b-container>
       <b-row style="min-height:48rem;" class="mt-5">
         <b-col>
-<!--          TODO: добавить хлебных крошек-->
-<!--          TODO: допилить верстку страницы "текст и название страницы"-->
-          <h1>Link {{$route.params.url}}</h1>
-          <h1>Link {{ link }}</h1>
+          <!--          TODO: добавить хлебных крошек, см. ISS-21 -->
+          <h1>{{ name }}</h1>
+
+          <p v-html="description"></p>
+
         </b-col>
       </b-row>
     </b-container>
@@ -14,30 +15,39 @@
 </template>
 
 <script>
+  import config from "../config";
+
   export default {
-    data: () => ({
-      link: ''
-    }),
-    async mounted() {
-      this.link = await this.$axios.$get(`https://api.marketplace.docker/page/${this.$route.params.url}?expand=_metaTags&_format=json`)
+    async fetch() {
+      const response = await this.$axios.$get(`${config.api_url}/page/${this.$route.params.url}?expand=_metaTags&_format=json`)
+      const {name, description, _metaTags} = response.data
+      this.name = name
+      this.description = description
+      this.metaTags = _metaTags
     },
+    data: () => ({
+      link: '',
+      name: '',
+      description: '',
+      metaTags: {}
+    }),
     head() {
-      //const {title, description, keywords} = this.link.meta;
+
       return {
-        title: `Rules`,
-        // TODO: настроить разборку метатегов
-        // meta: [
-        //   {
-        //     description: description,
-        //     keywords: keywords
-        //   } //локальные динамические метатеги
-        // ],
+        title: this.metaTags.title,
+        meta: [
+          {
+            description: this.metaTags.description,
+            keywords: this.metaTags.keywords
+          }
+        ],
       };
     },
     // TODO: сделать валидацию вводимых роутов
-    // validate({params}) {
-    //   return true //  /^\d+$/.test(params.id) например только цифры
-    // }
+    validate({params}) {
+      console.log(/^\w+$/.test(params.url))
+      return true
+    }
   }
 </script>
 
