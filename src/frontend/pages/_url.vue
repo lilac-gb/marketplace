2e63c535dedfba25925e05e5e8407d59cf580fc0
dannilsan <dannilsan@gmail.com>
2020-07-12 03:26:53 +0700

    #16 - rebase with develop branch

--- src/frontend/pages/_url.vue
+++ src/frontend/pages/_url.vue
@@ -1,13 +1,12 @@
 <template>
   <section>
     <b-container>
-      <b-row style="min-height:48rem;" class="mt-5">
+      <b-row style="min-height: 48rem;" class="mt-5">
         <b-col>
           <!--          TODO: добавить хлебных крошек, см. ISS-21 -->
           <h1>{{ name }}</h1>
 
           <p v-html="description"></p>
-
         </b-col>
       </b-row>
     </b-container>
@@ -15,41 +14,40 @@
 </template>
 
 <script>
-  import config from "../config";
+import config from '../config';
 
-  export default {
-    async fetch() {
-      const response = await this.$axios.$get(`${config.api_url}/page/${this.$route.params.url}?expand=_metaTags&_format=json`)
-      const {name, description, _metaTags} = response.data
-      this.name = name
-      this.description = description
-      this.metaTags = _metaTags
-    },
-    data: () => ({
-      link: '',
-      name: '',
-      description: '',
-      metaTags: {}
-    }),
-    head() {
-
-      return {
-        title: this.metaTags.title,
-        meta: [
-          {
-            description: this.metaTags.description,
-            keywords: this.metaTags.keywords
-          }
-        ],
-      };
-    },
-    // TODO: сделать валидацию вводимых роутов
-    validate({params}) {
-      return /^\w+$/.test(params.url)
-    }
-  }
+export default {
+  async fetch() {
+    const response = await this.$axios.$get(
+      `${config.api_url}/page/${this.$route.params.url}?expand=_metaTags&_format=json`
+    );
+    const { name, description, _metaTags } = response.data;
+    this.name = name;
+    this.description = description;
+    this.metaTags = _metaTags;
+  },
+  data: () => ({
+    link: '',
+    name: '',
+    description: '',
+    metaTags: {},
+  }),
+  head() {
+    return {
+      title: this.metaTags.title,
+      meta: [
+        {
+          description: this.metaTags.description,
+          keywords: this.metaTags.keywords,
+        },
+      ],
+    };
+  },
+  // TODO: сделать валидацию вводимых роутов
+  validate({ params }) {
+    return /^\w+$/.test(params.url);
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
