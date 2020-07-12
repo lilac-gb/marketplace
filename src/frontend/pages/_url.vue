9b7f7708c1f8263d0826e3510cd6f2ff8fafb947
dannilsan <dannilsan@gmail.com>
2020-07-13 03:57:40 +0700

    #16 - fix ISS-16

---
+++ src/frontend/pages/_url.vue
@@ -0,0 +1,52 @@
+<template>
+  <section>
+    <b-container>
+      <b-row style="min-height: 48rem;" class="mt-5">
+        <b-col>
+          <h1>{{ name }}</h1>
+
+          <p v-html="description"></p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+import config from '../config';
+
+export default {
+  async fetch() {
+    const response = await this.$axios.$get(
+      `${config.api_url}/page/${this.$route.params.url}?expand=_metaTags&_format=json`
+    );
+    const { name, description, _metaTags } = response.data;
+    this.name = name;
+    this.description = description;
+    this.metaTags = _metaTags;
+  },
+  data: () => ({
+    link: '',
+    name: '',
+    description: '',
+    metaTags: {},
+  }),
+  head() {
+    return {
+      title: this.metaTags.title,
+      meta: [
+        {
+          description: this.metaTags.description,
+          keywords: this.metaTags.keywords,
+        },
+      ],
+    };
+  },
+  // TODO: сделать валидацию вводимых роутов
+  validate({ params }) {
+    return /^\w+$/.test(params.url);
+  },
+};
+</script>
+
+<style scoped></style>
