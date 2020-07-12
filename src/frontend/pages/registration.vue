9f45a51f64b6799971540cdb89de6e205fba93f3
IlyesAkb <zenzor37rus@yandex.ru>
2020-07-12 12:42:57 +0300

    add registration form

---
+++ src/frontend/pages/registration.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container>
+    <RegistrationForm />
+  </b-container>
+</template>
+
+<script>
+import RegistrationForm from '@/components/Forms/RegistrationForm';
+
+export default {
+  name: 'Registration',
+  layout: 'empty',
+  components: { RegistrationForm },
+};
+</script>
+
+<style scoped></style>
