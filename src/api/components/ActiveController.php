8326dcab92b3ff94fce731b94185fb22972d8cd6
rogozhuk <resmedia@ya.ru>
2020-07-14 22:16:36 +0300

    #7 - Cors fix

--- src/api/components/ActiveController.php
+++ src/api/components/ActiveController.php
@@ -50,6 +50,14 @@ class ActiveController extends BaseActiveController
 
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::class,
+            'cors' => [
+                'Origin' => ['http://localhost:3000', 'https://marketplace.docker'],
+                'Access-Control-Request-Method' => ['POST', 'PUT', 'GET', 'DELETE'],
+                'Access-Control-Request-Headers' => ['X-Wsse'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 3600,
+                'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+            ],
         ];
 
         $behaviors['authenticator'] = [
--- src/api/components/Controller.php
+++ src/api/components/Controller.php
@@ -25,6 +25,14 @@ class Controller extends \yii\rest\Controller
 
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::class,
+            'cors' => [
+                'Origin' => ['http://localhost:3000', 'https://marketplace.docker'],
+                'Access-Control-Request-Method' => ['POST', 'PUT', 'GET', 'DELETE'],
+                'Access-Control-Request-Headers' => ['X-Wsse'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 3600,
+                'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+            ],
         ];
 
         $behaviors['authenticator'] = [
