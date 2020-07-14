04d76a084f6d962613c9ce9c146b4cc56ee5ac60
rogozhuk <resmedia@ya.ru>
2020-07-14 22:18:39 +0300

    #7 - Cors fix

--- src/api/components/Controller.php
+++ src/api/components/Controller.php
@@ -26,8 +26,8 @@ class Controller extends \yii\rest\Controller
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::class,
             'cors' => [
-                'Origin' => ['http://localhost:3000', 'https://marketplace.docker'],
-                'Access-Control-Request-Method' => ['POST', 'PUT', 'GET', 'DELETE'],
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['X-Wsse'],
                 'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Max-Age' => 3600,
