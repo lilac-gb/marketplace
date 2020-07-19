e2213a35a7b45b1393ca1e1d8f4bb534b8572e43
rogozhuk <resmedia@ya.ru>
2020-07-19 15:15:08 +0300

    #34 - Add env

---
+++ src/environments/prod/backend/config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'support@resmedia.ru',
+    'senderEmail' => 'feedback@resmedia.ru',
+    'domainBackend' => 'https://office.resmedia.ru',
+    'domainFrontend' => 'https://resmedia.ru',
+    'urlToSyncTranslit' => '/js/synctranslit.js?v8',
+    'urlToCke' => '/js/config-cke.js?_v6',
+];
