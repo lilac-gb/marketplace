5ac33f4e85ba86956a2f030f182305667b02c4b1
Kate Grigoryeva <kate23april@yandex.ru>
2020-07-27 18:04:04 +0300

    #8 - Add password recovery page

--- src/frontend/pages/recovery.vue
+++ src/frontend/pages/recovery.vue
@@ -2,50 +2,58 @@
   <b-container>
     <b-row style="min-height: 48rem;" class="mt-5">
       <b-col>
-        <div class="d-flex justify-content-center">
-          <h2 class="mx-auto mb-5">Восстановление пароля</h2>
-        </div>
-        <b-form class="w-50 m-auto" @submit.prevent="onSubmit">
-          <b-form-group label="Email" label-for="email">
-            <b-form-input
-              id="email"
-              v-model="email"
-              type="email"
-              placeholder="Введите email"
-            ></b-form-input>
-          </b-form-group>
-          <b-button type="submit" variant="primary">Отправить</b-button>
-        </b-form>
+        <ValidationObserver v-slot="{ valid }">
+          <b-form class="w-50 m-auto" @submit.prevent="onSubmit">
+            <h1 class="mb-5">Восстановление пароля</h1>
+            <ValidationProvider v-slot="{ errors }" rules="required|email" name="email">
+              <b-form-group label="Email" label-for="email">
+                <b-form-input
+                  id="email"
+                  v-model="email"
+                  type="email"
+                  placeholder="Введите email"
+                ></b-form-input>
+                <span v-if="errors[0]">
+                  {{ errors[0] }}
+                </span>
+              </b-form-group>
+            </ValidationProvider>
+            <b-button type="submit" variant="primary" :disabled="!valid">Отправить</b-button>
+          </b-form>
+        </ValidationObserver>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-// import config from '@/config';
-// import { constructUrl } from '@/shared/api';
+import config from '@/config';
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
 
 export default {
+  components: { ValidationObserver, ValidationProvider },
   data() {
     return {
       email: '',
     };
   },
   methods: {
-    async onSubmit() {
-      console.log(this.email);
-      // https://api.marketplace.docker/user/restore-password передать нужно email
-      // let response = await this.$http.post(
-      //   // (url: Request | URL | string, options?: Omit<Options, 'body'>)
-      //   // constructUrl(`${config.api_url}/user/restore-password`, this.email)
-      //   // (url: Request | URL | string, body?: RequestBody, options?: Options)
-      //   constructUrl(`${config.api_url}/user/restore-password`, this.email)
-      // );
-      // // email попадает в v1/controllers/UserController.php actionRecovery()
-      // if (response.ok) {
-      //   // если сервер отправил письмо и вернул 'statusCode' => 200
-      //   this.$router.push('/');
-      // } else {}
+    onSubmit() {
+      try {
+        const response = this.$axios.post(`${config.api_url}/user/restore-password`,
+          {
+            email: this.email,
+          }
+        );
+        console.log(response);
+        // if (response.statusCode === 200) {
+        //   // this.$router.push('/');
+        // } else {
+        //   console.log(response.error);
+        // }
+      } catch (e) {
+        console.log(e);
+      }
     },
   },
   head() {
@@ -53,6 +61,7 @@ export default {
       title: 'Password recovery',
     };
   },
+  layout: 'empty',
 };
 </script>
 
