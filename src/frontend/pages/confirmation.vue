06850c35b6b2ff5bb69c708371b42b5cc1bddbb2
dannilsan <dannilsan@gmail.com>
2020-07-29 00:14:45 +0700

    #9 - fix validation markers

--- src/frontend/pages/confirmation.vue
+++ src/frontend/pages/confirmation.vue
@@ -7,28 +7,31 @@
 </template>
 
 <script>
-  import axios from 'axios';
-  import config from "../config";
+import axios from 'axios';
+import config from '../config';
 
-  export default {
-    name: 'Confirmation',
-    layout: 'default',
-    fetch({query, store, redirect}) {
-      if (query.hash && query.id) {
-        axios.post(`${config.api_url}/user/${query.id}/confirm-email`, {hash: query.hash})
-          .then(response => {
-            if (response.data && response.data.statusCode === 200) {
-              store.commit('updateMessage', 'Email успешно подтвержден!');
-              redirect('/cabinet');
-            }
-          })
-          .catch(error => {
-            if (error.response && error.response.data) {
-              store.commit('updateMessage', error.response.data.data.message);
-              redirect('/');
-            }
-          });
-      }
-    },
-  };
+export default {
+  name: 'Confirmation',
+  layout: 'default',
+  fetch({ query, store, redirect }) {
+    if (query.hash && query.id) {
+      axios
+        .post(`${config.api_url}/user/${query.id}/confirm-email`, {
+          hash: query.hash,
+        })
+        .then((response) => {
+          if (response.data && response.data.statusCode === 200) {
+            store.commit('updateMessage', 'Email успешно подтвержден!');
+            redirect('/cabinet');
+          }
+        })
+        .catch((error) => {
+          if (error.response && error.response.data) {
+            store.commit('updateMessage', error.response.data.data.message);
+            redirect('/');
+          }
+        });
+    }
+  },
+};
 </script>
